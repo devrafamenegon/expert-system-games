9c3655bbef8427dfe4e7b5ce740835d30d381b69
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-02-25 22:55:59 -0300

    feat: add utils

---
+++ utils.py
@@ -0,0 +1,23 @@
+def indique_genero():
+    return input("Informe um gênero: ").capitalize()
+
+
+def indique_grau():
+    return int(input("Informe o quanto você gosta desse gênero (0 a 10): "))
+
+
+def deseja_adicionar_genero():
+    respondeu = False
+
+    while not respondeu:
+        continuar = input("\nDeseja adicionar outro gênero? [S] ou [N]: ").lower()
+
+        if continuar == "s":
+            respondeu = True
+            return True
+
+        if continuar == "n":
+            respondeu = True
+            return False
+
+        print("\nOpção inválida.")
