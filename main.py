4308a604008d64ac8edbad8152d859585bdeac19
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-03-29 16:41:14 -0300

    feat: add comments into main file

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+from unidecode import unidecode
 from utils import indique_genero, indique_grau, deseja_adicionar_genero
 from database import BD, zerar_ocorrencias, mostrar_generos
 
+
 zerar_ocorrencias()
 
+# INICIALIZAR VARIÁVEIS
 generosIndicados = []
 continuar = True
 
 mostrar_generos()
 
+# COLETAR DADOS
 while continuar:
     generosIndicados.append((indique_genero(), indique_grau()))
     continuar = deseja_adicionar_genero()
@@ -16,28 +20,26 @@ while continuar:
 for jogo in BD:
     for generoIndicado, grauIndicado in generosIndicados:
         for jogo_genero in jogo["GENERO"]:
-            if jogo_genero["NOME"] == generoIndicado:
+            if unidecode(jogo_genero["NOME"].lower()) == unidecode(generoIndicado.lower()):
                 jogo["OCORRENCIAS"] += grauIndicado * jogo_genero["GRAU"]
 
-# VERIFICAÇÃO DE MATCHING CONSIDERANDO O TOTAL DE GENEROS
+# MOSTRAR GÊNEROS INDICADOS
 print("\n*************************************")
 print("\nRESULTADOS PARA OS GÊNEROS: ")
-
 for generoIndicado, grauIndicado in generosIndicados:
     print(f" - {generoIndicado} (Grau: {grauIndicado})")
 
-print("\nPROBABILIDADE DE MATCHING CONSIDERANDO O TOTAL DE GÊNEROS: ")
-
-# Encontrar a maior ocorrência
+# ENCONTRAR A MAIOR OCORRÊNCIA
 maior_ocorrencia = max(jogo["OCORRENCIAS"] for jogo in BD)
 
+# CALCULAR PERCENTUAL
 for jogo in BD:
     jogo["PERCENTUAL"] = (jogo["OCORRENCIAS"] / maior_ocorrencia) * 100
-    print(f"{jogo['PERCENTUAL']:.2f}% = {jogo['TITULO']}")
 
 # ORDENAR JOGOS POR PERCENTUAL DE MATCHING
 jogos_ordenados = sorted(BD, key=lambda x: x["PERCENTUAL"], reverse=True)
 
+# MOSTRAR JOGOS ORDENADOS
 print("\nJOGOS ORDENADOS POR PERCENTUAL DE MATCHING:")
 for jogo in jogos_ordenados:
     print(f"{jogo['PERCENTUAL']:.2f}% = {jogo['TITULO']}")
